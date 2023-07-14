0be26e50a843035c1ee48a87285e1d9abf57e79a
mylle <myllersilva1310@gmail.com>
2023-07-13 22:26:17 -0300

    Update Tarefa.cs

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -10,7 +10,7 @@ namespace TrilhaApiDesafio.Models
         public int Id { get; set; }
         public string Titulo { get; set; }
         public string Descricao { get; set; }
-        public DateTime Data { get; set; }
+        public DateTime DataCriacao { get; set; }
         public EnumStatusTarefa Status { get; set; }
     }
 }
